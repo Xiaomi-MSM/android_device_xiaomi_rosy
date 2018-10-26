0a568155242ca47d292237e68200212de5985470
ZIM <zeustreker@gmail.com>
2018-10-26 18:56:18 +0300

    Update rosy

--- prop.mk
+++ prop.mk
@@ -7,13 +7,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.media_vol_steps=25 \
 ro.config.vc_call_vol_steps=7
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.exif.make=Xiaomi \
+persist.sys.exif.model=Redmi 5\
+	
 # DT2W (rosy)
 # value = 1 means enabled
 # value = 0 means disabled
 PRODUCT_PROPERTY_OVERRIDES += \
 dt2w.enabled=1
-
-# Exif info for camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.exif.make=Xiaomi \
-    persist.sys.exif.model=Redmi 5
